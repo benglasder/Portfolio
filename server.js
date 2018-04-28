fe3a09546c1e09440f800f4310efe8aad3bf6ef8
Ben Glasder <bglasder@gmail.com>
2018-04-27 23:20:48 -0500

    added original site content

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 // Setup Port
 var port = process.env.PORT || 3000;
 
+app.use('/images', express.static(path.join(__dirname, '/images')));
+app.use('/css', express.static(path.join(__dirname, '/css')));
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
@@ -28,6 +30,13 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index.ejs');
 })
-app.get('/learnMore', (req, res) => {
-    res.render('learnMore.ejs');
+
+
+app.get('/aboutMe', (req, res) => {
+    res.render('aboutMe.ejs');
 })
+
+app.get('/download', function(req, res){
+    var file = __dirname + '/uploads/Resume.pdf';
+    res.download(file); // Set disposition and send it.
+});
