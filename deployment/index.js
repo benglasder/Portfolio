12bdbd3fdf602b33e535fa08e614bb9372e4976f
Ben Glasder <bglasder@gmail.com>
2018-04-27 19:29:38 -0500

    changeToServer

--- deployment/index.js
+++ deployment/index.js
@@ -5,7 +5,7 @@ var NodeDeployment = require("codedeploy-scripts").NodeDeployment;
 var deployment = new NodeDeployment({
     appName: "portfolio",
     nodePort: "5020",
-    serverScript: "build/server/index.js",
+    serverScript: "build/server/server.js",
     secretBucket: "tqc-encrypted"
 });
 
