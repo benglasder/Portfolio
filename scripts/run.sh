decda700e267dced4207d1c387da35d6a8c585ce
Ben Glasder <bglasder@gmail.com>
2018-04-27 20:06:11 -0500

    changedTo3000

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,4 @@ echo "did this deploy?"
 
 cd /home/ec2-user/portfolio
 
-
-#!/bin/bash
-# sudo chmod 755 /var/www/server.js # optional
-# this will restart app/server on instance reboot
-crontab -l | { cat; echo "@reboot pm2 start server.js -i 0 --name \"portfolio\""; } | crontab -
-sudo pm2 stop node-app
-# actually start the server
-sudo pm2 start server.js -i 0 --name "portfolio"
+npm run start
