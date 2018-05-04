40d9b9f3e13f4166a601eb678dffde3c34933302
Ben Glasder <bglasder@gmail.com>
2018-05-04 13:59:15 -0500

    Remove log entry for log location

--- server/logger.js
+++ server/logger.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 const level = process.env.LOG_LEVEL || 'debug';
 
-console.log(process.env.LOG_LOCATION);
-
 const logger = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)({ json: false, timestamp: true }),
