568ce6857af5e98feff2515cd5383a1e1c59014b
Ben Glasder <bglasder@gmail.com>
2018-05-10 15:04:39 -0700

    Add toDoItem model

---
+++ models/toDoItem.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+logger = require('../server/logger');
+
+// to-do item schema
+var ToDoItemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        index: true
+    },
+    category: {
+        type: String,
+    },
+    priority: {
+        type: Number,
+    },
+    dateEntered: {
+        type: Date,
+    },
+    dateCompleted: {
+        type: Date,
+    },
+    assignedUser: {
+        type: String,
+    }
+});
+
+var ToDoItem = module.exports = mongoose.model('ToDoItem', ToDoItemSchema);
+
+module.exports.createToDoItem = function(newToDoItem, callback){
+    newTodoItem.save(callback);
+}
+
+module.exports.getAllToDoItems = function(req, res, callback){
+    logger.info('Get all ToDoItems');
+    ToDoItem
+        .find({})
+        .lean
+        .exec(function(err, result) {
+            return callback(result);
+        });
+}
