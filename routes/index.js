4a76f698fba0db811efd0c8870d7ef4b82acbe2e
Ben Glasder <bglasder@gmail.com>
2018-06-15 10:00:18 -0500

    Add IP to logs

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,28 @@ var router = express.Router();
 logger = require('../server/logger');
 
 
+
+
 // Get Homepage
 router.get('/', (req, res) => {
-    logger.info(`Home`);
-   res.render('index.ejs');
+
+    var ip = req.headers['x-forwarded-for'] ||
+	    req.connection.remoteAddress ||
+	    req.socket.remoteAddress ||
+	    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    logger.info(`Home ${ip}`);
+    res.render('index.ejs');
 });
 
 router.get('/aboutMe', (req, res) => {
-    logger.info('About me');
+
+    var ip = req.headers['x-forwarded-for'] ||
+	    req.connection.remoteAddress ||
+	    req.socket.remoteAddress ||
+	    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    logger.info(`About me ${ip}`);
     res.render('aboutMe.ejs');
 });
 
